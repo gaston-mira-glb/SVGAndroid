01f503244d240f2a9292fec3882d7885fb86abd6
Gaston Mira <gaston.mira@globant.com>
2016-02-12 14:21:57 -0300

    Innitial Commit

---
+++ app/src/main/java/com/example/gastonmira/svgandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gastonmira.svgandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
